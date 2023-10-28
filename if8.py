7283655728d17b14b9323f7e7de624d403b4f086
USER <hello@gsdadg.asdsadasd>
2023-10-28 16:06:07 +0300

    Hello

--- if8.py
+++ if8.py
@@ -1,9 +1,8 @@
 cher = input("Сколько вишен вы хотите съесть")
 cher = int(cher)
-
-if cher%10 == 1:
+if cher % 10 == 11 or cher >= 5 and cher <= 20:
+    print("Вы съели", cher, "вишен")
+elif cher == 1 or cher%10 == 1:
     print("Вы съели",cher,"вишню")
 elif cher >= 2 and cher <= 4:
     print("Вы съели",cher,"вишни")
-elif cher >=5 and cher <=20:
-    print("Вы съели", cher, "вишен")
